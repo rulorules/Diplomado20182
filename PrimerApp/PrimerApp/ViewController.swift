bec6b335dc1adabed799f56ecfc16b5cc9bb546b
d182_raul_j <d182_raul_j@macbookpro-12.local>
2018-03-02 16:08:15 -0600

    qwer

---
+++ PrimerApp/PrimerApp/ViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ViewController.swift
+//  PrimerApp
+//
+//  Created by d182_raul_j on 16/02/18.
+//  Copyright © 2018 d182_raul_j. All rights reserved.
+//
+//Correo profesor norberto@bonsan.io
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var questionLabel: UILabel!
+    @IBOutlet var answerLabel: UILabel!
+    
+    let questions: [String] = [
+        "What is 7+7?",
+        "What is the capital of Vermont?",
+        "What is cognac made from?"
+    ]
+    let answers: [String] = [
+        "14",
+        "Montpelier",
+        "Grapes" ]
+    var currentQuestionIndex: Int = 0
+    
+    @IBAction func showNextQuestion(_ sender: UIButton) {
+        currentQuestionIndex += 1
+        if currentQuestionIndex == questions.count {
+            currentQuestionIndex = 0
+        }
+        let question: String = questions[currentQuestionIndex]
+        questionLabel.text = question
+        answerLabel.text = "???"
+    }
+    @IBAction func showAnswer(_ sender: UIButton) {
+        let answer: String = answers[currentQuestionIndex]
+        answerLabel.text = answer
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        questionLabel.text = questions[currentQuestionIndex]
+    }
+}
+

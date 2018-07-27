b6801bffd148f4121f8252870a25a6561088ab7f
Jorge Alba <jorgealba@MacBook-Air-de-Jorge.local>
2018-07-27 01:22:05 -0500

    Actualizada consulta de lugares

---
+++ Evaluacion/Consulta de lugares/Consulta de sitios/BarAnnotation.swift
@@ -0,0 +1,37 @@
+//
+//  BarAnnotation.swift
+//  Consulta de sitios
+//
+//  Created by Jorge Alba on 27/07/18.
+//  Copyright © 2018 d182_raul_j. All rights reserved.
+//
+
+import Foundation
+import MapKit
+import Contacts
+class BarAnnotation: NSObject,MKAnnotation {
+    let title: String?
+    //let locationName: String?
+    let recid: String
+    let coordinate: CLLocationCoordinate2D
+    
+    init(title: String, recid: String, coordinate: CLLocationCoordinate2D){
+        self.title = title
+        //self.locationName = locationName
+        self.recid = recid
+        self.coordinate = coordinate
+        super.init()
+    }
+    
+    //    var subtitle: String?{
+    //        return locationName
+    //    }
+    
+    func mapItem() -> MKMapItem {
+        //let addressDict = [CNPostalAddressStreetKey: subtitle!]
+        let placemark = MKPlacemark(coordinate: coordinate)
+        let mapItem = MKMapItem(placemark: placemark)
+        mapItem.name = title
+        return mapItem
+    }
+}
